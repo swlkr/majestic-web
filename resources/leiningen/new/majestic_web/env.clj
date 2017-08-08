dd0396d6d6b141a9685d86ef8eb5572c5aee01c1
Sean Walker <swlkr.rbl@gmail.com>
2017-08-08 07:50:05 -0700

    Remove env.clj

--- resources/leiningen/new/majestic_web/env.clj
+++
@@ -1,7 +0,0 @@
-(ns {{name}}.env
-  (:require [environ.core :as environ]
-            [{{name}}.utils :refer [str->int]]))
-
-(def port (str->int (environ/env :port)))
-(def database-url (environ/env :database-url))
-(def secret (environ/env :secret))
